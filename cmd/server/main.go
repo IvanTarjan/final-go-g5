1f5d023b213fa22cd0fd026acdf984f8ff0ad219
Andrés Galván <8il.andre@gmail.com>
2023-11-29 23:16:01 -0300

    First documentation attempt

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
 
+// @BasePath /api/v1
+
+// PingExample godoc
+// @Summary ping example
+// @Description do ping
+func main() {
+	r := gin.New()
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	r.Run()
+
+	// Create a new Gin engine.
+	router := gin.New()
+
+	// Add the swagger handler.
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 }
